85b6d715b6711efdcd62cf846292884450927ecd
evoituriez <edouard.voituriez@gmail.com>
2017-12-04 18:44:53 +0100

    Avant de modifier la css

---
+++ sites/default/files/php/twig/5a2076f7ef3fc_messages-icon-status.svg_Fn-CeiccRjozdC14KEmSdIirs/Adcoa1n7iC3J3Y7OLtnuE9arlYIIzii9l8S77WR8JM0.php
@@ -0,0 +1,71 @@
+<?php
+
+/* @casd_nepal_theme/components/messages/messages-icon-status.svg */
+class __TwigTemplate_dda3c4bfb694046a08a737c4ba77534b5ffaa24a61101c90f1f40c6dd4706f3f extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array();
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('Twig_Extension_Sandbox')->checkSecurity(
+                array(),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setSourceContext($this->getSourceContext());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 1
+        echo "<svg version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"24\" height=\"24\" viewBox=\"0 0 64 64\">
+  <path d=\"M54 8l-30 30-14-14-10 10 24 24 40-40z\" fill=\"#000000\"></path>
+</svg>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "@casd_nepal_theme/components/messages/messages-icon-status.svg";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  43 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("", "@casd_nepal_theme/components/messages/messages-icon-status.svg", "/Applications/MAMP/htdocs/Site_CASD_Nepal/themes/custom/casd_nepal_theme/components/components/messages/messages-icon-status.svg");
+    }
+}
